c2300cce13055db247bd6a18801a49863f7fa563
unknown <Administrador@AI01.MDYMG.COM>
2024-12-14 15:59:40 -0500

    14/12/2024 16.00

--- src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  horas: any[] = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.listHOras();
+  }
+
+  listHOras(){
+    this.http.get('http://10.200.40.71:8000/api/screens').subscribe((resp: any) => {
+      this.horas = resp.data;
+      console.log(this.horas[0]);
+    });
+  }
 }
